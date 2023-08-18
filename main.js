eac74d3fe28b861881da4d720a6e77e5d1985b8d
weiweiwei-hub <congwei_huang@163.com>
2023-08-18 23:54:26 +0800

    (bug) 主题切换问题修复

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ window.addEventListener("resize", () => {
 
 const toggler = document.getElementById("theme-toggle");
 
-toggler.addEventListener("change", () => {
+toggler.addEventListener("change", function () {
   if (this.checked) {
     document.body.classList.add("dark");
   } else {
